e2f0e0550e77066d8ef33e73637c28e3a459e878
Sandip <sandip.bagale34@gmail.com>
2023-11-01 14:58:30 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+
+function App() {
+
+  const name = "Mansh"
+  
+
+  return (
+    <>
+     <h1 style={{color:"blue"}}>Hello world</h1>
+     <h2>Bye world</h2>
+     <p style={{fontSize: 20}}>Manish</p>
+     <h5>Age: {11+10}</h5>
+     <h2>{name}</h2>
+    </>
+  )
+}
+
+export default App
